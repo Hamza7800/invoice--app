315192a15c5f14fce6c203821fd4f06c1a9c2a56
Hamza Farooq <hamzakhan12777@gmail.com>
2023-08-18 22:31:10 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import path from 'path';
 
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,14 +35,14 @@ app.use('/api/users', userRouter);
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
-} else {
-  app.get('/', (req, res) => {
-    res.send('hello from the server');
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('hello from the server');
+//   });
+// }
 
 
 // Error Handling
