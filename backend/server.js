69b44bd4257770fcbd1cb603d5913ada346bb5d9
Hamza Farooq <hamzakhan12777@gmail.com>
2023-08-18 20:06:44 +0500

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import connectDB from './config/connectDB.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import invoiceRouter from './router/invoiceRouter.js';
 import userRouter from './router/userRouter.js';
+import path from 'path';
+
 
 const app = express();
 // app.use(cors());
@@ -30,13 +32,22 @@ const port = process.env.PORT;
 //Routes 
 app.use('/api/invoice', invoiceRouter);
 app.use('/api/users', userRouter);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => {
+    res.send('hello from the server');
+  });
+}
+
+
 // Error Handling
 app.use(notFound);
 app.use(errorHandler);
-
-app.get('/', (req, res) => {
-  res.send('hello from the server');
-});
 
 app.listen(port, () => console.log(`Server running on port ${ port }`));
 
